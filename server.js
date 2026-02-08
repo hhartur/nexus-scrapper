972304cdd2b4a2ca3091aa634d870d43acefc82f
NpcXYZ0 <hharturcarmello09@gmail.com>
2026-02-07 22:29:19 -0300

    .

--- server.js
+++ server.js
@@ -268,7 +268,7 @@ app.get('/image', async c => {
 
   } catch (error) {
     console.error('Erro:', error)
-    return c.text('upload failed', 500)
+    return c.text(`upload failed, ${error}`, 500)
   }
 })
 
